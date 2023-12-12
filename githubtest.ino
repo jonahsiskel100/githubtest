05b0d93f8f8bc7ec6f9ead37bc7c12306d767033
jonahsiskel100 <jes9849@nyu.edu>
2023-12-12 12:51:40 -0500

    added 2nd led

--- githubtest.ino
+++ githubtest.ino
@@ -1,10 +1,10 @@
 int ledPin100 = 2;
-
+int ledPin500 = 3;
 void setup() {
   pinMode(ledPin100, OUTPUT);
+  pinMode(ledPin500, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
